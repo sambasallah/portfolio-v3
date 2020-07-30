2a01c37d9c6febc90bfefd1591e4ec86847e38c9
cweave <cweav3r@gmail.com>
2020-07-29 21:04:30 -0400

    contact and beginning of projects

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,43 +1,121 @@
 <template>
 	<section v-if="isContactDrawerOpen" class="contact-section">
-		<h1>Contact</h1>
+		<h1>Get in Touch</h1>
 
 		<div class="contact-section__grid-container grid-container">
-			<div class="contact"></div>
+			<section class="contact">
+				<p>
+					<strong>New Project?</strong> Great! I'm currently accepting new projects and would love to hear about yours.
+				</p>
+				<p>
+					<strong>Interested in More?</strong> Want to see more of my work? Let me know and I'll gladly send more examples!
+				</p>
+				<h3>Connect with me!</h3>
 
-			<div class="form">
+				<ul class="contact-social-links">
+					<li>
+						<a
+							href="https://github.com/cweave"
+							target="_blank"
+							title="Check out Christa's Github repositories"
+						>
+							<svg viewBox="0 0 16 15" role="img" labelledby="githubTitle">
+								<title id="githubTitle">Github logo</title>
+								<use xlink:href="../assets/svgs/github.svg#github" />
+							</svg>
+						</a>
+					</li>
+					<li>
+						<a href="https://codepen.io/cweave" target="_blank" title="Follow Christa on CodePen">
+							<svg viewBox="0 0 16 15" role="img" labelledby="codepenTitle">
+								<title id="codepenTitle">Codepen logo</title>
+								<use xlink:href="../assets/svgs/codepen.svg#codepen" />
+							</svg>
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://www.linkedin.com/in/weaverchrista/"
+							target="_blank"
+							title="Connect with Christa on LinkedIn"
+						>
+							<svg viewBox="0 0 16 15" role="img" labelledby="linkedInTitle">
+								<title id="linkedInTitle">LinkedIn logo</title>
+								<use xlink:href="../assets/svgs/linkedin.svg#linkedin" />
+							</svg>
+						</a>
+					</li>
+				</ul>
+			</section>
+
+			<section class="form">
+				<h2>Send a Message</h2>
 				<form>
-					<div class="form-input">
-						<label for="name">
-							Name
-							<span class="req">*</span>
-						</label>
-						<input type="text" id="name" name="name" required @keyup="activeText($event)" />
+					<div class="form-col">
+						<div class="form-input">
+							<label for="name">
+								Name
+								<span class="req">*</span>
+							</label>
+							<input
+								type="text"
+								id="name"
+								name="name"
+								@keyup="handleKeyUp"
+								@blur="handleBlur"
+								@focus="handleFocus"
+								required
+							/>
+						</div>
+						<div class="form-input">
+							<label for="email">
+								Email
+								<span class="req">*</span>
+							</label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								@keyup="handleKeyUp"
+								@blur="handleBlur"
+								@focus="handleFocus"
+								required
+							/>
+						</div>
+						<div class="form-input">
+							<label for="phone">Phone</label>
+							<input
+								type="tel"
+								id="email"
+								name="phone"
+								@keyup="handleKeyUp"
+								@blur="handleBlur"
+								@focus="handleFocus"
+								@input="formatPhone($event)"
+							/>
+						</div>
 					</div>
-					<div class="form-input">
-						<label for="email">
-							Email
-							<span class="req">*</span>
-						</label>
-						<input type="email" id="email" name="email" required @keyup="activeText($event)" />
-					</div>
-					<div class="form-input">
-						<label for="phone">
-							Phone
-							<span class="req">*</span>
-						</label>
-						<input type="tel" id="email" name="phone" @input="formatPhone($event)" />
-					</div>
-					<div class="form-input">
-						<label for="message">
-							Leave a brief description of your project
-							<span class="req">*</span>
-						</label>
-						<textarea required name="message" id="message"></textarea>
+					<div class="form-col">
+						<div class="form-input">
+							<label for="message">
+								Leave a brief description of your project
+								<span class="req">*</span>
+							</label>
+							<textarea
+								required
+								name="message"
+								id="message"
+								@keyup="handleKeyUp"
+								@blur="handleBlur"
+								@focus="handleFocus"
+							></textarea>
+						</div>
+						<button type="submit" class="btn btn--primary">Send</button>
 					</div>
 				</form>
-			</div>
+			</section>
 		</div>
+		<p>handcrafted with &#x2764; {{ new Date().getFullYear() }}</p>
 	</section>
 </template>
 
@@ -62,27 +140,28 @@ export default {
 				? digit[1]
 				: `(${digit[1]}) ${digit[2]}${digit[3] ? `-${digit[3]}` : ''}`;
 		},
-		activeText(e) {
-			console.log(e.type);
+		handleKeyUp(e) {
 			const label = e.target.previousSibling;
-			if (e.type === 'keyup') {
-				if (e.target.value === '') {
-					label.classList.remove('active highlight');
-				} else {
-					label.classList.add('active highlight');
-				}
-			} else if (e.type === 'blur') {
-				if (e.target.value === '') {
-					label.classList.remove('active highlight');
-				} else {
-					label.classList.remove('highlight');
-				}
-			} else if (e.type === 'focus') {
-				if (e.target.value === '') {
-					label.classList.remove('highlight');
-				} else if (e.target.value !== '') {
-					label.classList.add('highlight');
-				}
+			if (e.target.value === '') {
+				label.classList.remove('active', 'highlight');
+			} else {
+				label.classList.add('active', 'highlight');
+			}
+		},
+		handleBlur(e) {
+			const label = e.target.previousSibling;
+			if (e.target.value === '') {
+				label.classList.remove('active', 'highlight');
+			} else {
+				label.classList.remove('highlight');
+			}
+		},
+		handleFocus(e) {
+			const label = e.target.previousSibling;
+			if (e.target.value === '') {
+				label.classList.remove('highlight');
+			} else if (e.target.value !== '') {
+				label.classList.add('highlight');
 			}
 		}
 	}
@@ -90,18 +169,6 @@ export default {
 </script>
 
 <style lang="scss">
-.contact-section {
-	height: 50vh;
-	width: 100%;
-	background-color: $slate;
-	position: fixed;
-	bottom: 0;
-	z-index: 10;
-	overflow: hidden;
-	animation: slide-in-blurred-bottom 0.5s both;
-	transform: all ease 0.5s;
-}
-
 @keyframes slide-in-blurred-bottom {
 	0% {
 		transform: translateY(1000px) scaleY(2.5) scaleX(0.2);
@@ -118,57 +185,107 @@ export default {
 }
 
 .contact-section {
+	height: 50vh;
+	width: 100%;
+	background-color: $slate;
+	position: fixed;
+	bottom: 0;
+	z-index: 10;
+	overflow: hidden;
+	animation: slide-in-blurred-bottom 0.5s both;
+	transform: all ease 0.5s;
+
 	&__grid-container {
 		color: $black;
 		margin: 1em;
 
-		> div {
+		> section {
 			background: none;
 			box-shadow: none;
 		}
 
-		& > div:nth-child(1) {
+		& > section:nth-child(1) {
 			grid-area: contact;
+
+			.contact-social-links {
+				display: inline-flex;
+				list-style-type: none;
+				padding: 0;
+				margin: 0;
+
+				li {
+					a {
+						background-image: none;
+						transition: color 0.2s ease-in;
+
+						&:hover,
+						&:focus,
+						&:active {
+							color: $mint;
+						}
+					}
+
+					&:not(:last-of-type) {
+						margin-right: 10px;
+					}
+				}
+
+				svg {
+					width: 30px;
+				}
+			}
 		}
-		& > div:nth-child(2) {
+
+		& > section:nth-child(2) {
 			grid-area: form;
+			padding: 0;
 
 			form {
 				display: flex;
-				flex-direction: column;
 
-				.form-input {
-					position: relative;
-					margin-bottom: 40px;
+				.form {
+					&-col {
+						&:first-of-type {
+							margin-right: 1rem;
+						}
+
+						.btn {
+							float: right;
+						}
+					}
+
+					&-input {
+						position: relative;
+						margin-bottom: 28px;
+					}
 				}
 
 				label {
+					font-size: 1.1rem;
 					font-weight: 700;
 					position: absolute;
-					transform: translateY(6px);
-					left: 13px;
-					color: rgba($black, 0.5);
+					transform: translateY(10px);
+					left: 1rem;
+					color: $light-slate;
 					transition: all 0.25s ease;
 					-webkit-backface-visibility: hidden;
 					pointer-events: none;
-					font-size: 22px;
+					line-height: 1.15;
+
 					.req {
 						margin: 2px;
 						color: $purple;
 					}
 
-					&:not(:first-of-type) {
-						margin-top: 10px;
-					}
-
 					&.highlight {
-						color: $white;
+						color: $purple;
 					}
 
 					&.active {
-						transform: translateY(50px);
+						transform: translateY(46px);
 						left: 2px;
-						font-size: 14px;
+						font-size: 1rem;
+
 						.req {
 							opacity: 0;
 						}
@@ -177,21 +294,26 @@ export default {
 
 				input,
 				textarea {
+					font-family: $sans-serif;
 					display: block;
-					font-size: 22px;
+					font-size: 1.25rem;
+					color: $black;
 					width: 100%;
-					height: 100%;
-					padding: 5px 10px;
+					padding: 0.5rem 1rem;
 					background: none;
-					background-image: none;
-					border: 1px solid $slate;
-					color: $white;
-					border-radius: 0;
+					border: 1px solid rgba($black, 0.4);
+					border-radius: 4px;
 					transition: border-color 0.25s ease, box-shadow 0.25s ease;
-					&:focus {
+
+					&:focus,
+					&:hover {
 						outline: 0;
 						border-color: $purple;
 					}
+				}
+
+				textarea {
+					margin: 0;
 				}
 			}
 		}
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,4 +1,42 @@
 <template>
-    <div class="projects">
-    </div>
+	<div class="projects-content">
+		<h1>Projects</h1>
+		<div>
+			<ul>
+				<li v-for="event in gitEvents" :key="event.index">{{ event.type }} {{ event.repo }}</li>
+			</ul>
+		</div>
+	</div>
 </template>
+
+<script>
+const getGitEvents = () => {
+	const api = 'https://api.github.com/users/cweave/events';
+	return fetch(api).then((response) => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		return response.json();
+	});
+};
+export default {
+	name: 'Projects',
+	data() {
+		return {
+			gitEvents: []
+		};
+	},
+	mounted() {
+		getGitEvents().then((result) => {
+			result.forEach((event) => {
+				if (event.type !== 'WatchEvent' && event.type !== 'IssueCommentEvent') {
+					this.gitEvents.push({
+						type: event.type,
+						repo: event.repo
+					});
+				}
+			});
+		});
+	}
+};
+</script>
