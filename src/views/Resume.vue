a3dae3440494f2e4ed0f400898b21313e4ef4b58
cweave <cweav3r@gmail.com>
2020-08-03 01:23:43 -0400

    resume

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,8 +1,13 @@
 <template>
 	<div>
 		<h1>Resume</h1>
-		<div v-for="job in experience" :key="job.index">
-			<div class="position">
+		<p>
+			Frontend developer with a dedication for providing the best possible experience to end users. Proven
+			record for taking the initiative and learning new technologies and languages. Committed to learning
+			and sharing knowledge with team members and moving towards a common goal of success.
+		</p>
+		<div v-for="job in experience" :key="job.index" class="position">
+			<div class="title">
 				<h2>{{ job.position }}</h2>
 				<h3>{{ job.company }}</h3>
 			</div>
@@ -11,7 +16,7 @@
 			</ul>
 
 			<p>{{ job.description }}</p>
-			<ul>
+			<ul class="task-list no-list">
 				<li v-for="task in job.tasks" :key="task.index">{{ task }}</li>
 			</ul>
 		</div>
@@ -38,8 +43,19 @@ export default {
 
 <style lang="scss">
 .position {
+	padding: 0 0 1em 1em;
+	margin-bottom: 1em;
+	border-left-width: 2px;
+	border-right-width: 0;
+	border-style: solid;
+	border-image: linear-gradient(to bottom, $skyblue, rgba(0, 0, 0, 0)) 1 100%;
+}
+
+.title {
 	display: flex;
+	flex-wrap: wrap;
 	align-items: center;
+	margin-bottom: 1em;
 
 	h2 {
 		margin: 0;
@@ -48,9 +64,15 @@ export default {
 			content: '\2014';
 		}
 	}
+
+	h3 {
+		margin-left: 0;
+		font-style: italic;
+	}
 }
 .technologies-list {
 	display: inline-flex;
+	flex-wrap: wrap;
 
 	li {
 		font-size: 12px;
@@ -59,9 +81,26 @@ export default {
 		padding: 2px 10px;
 		background-color: rgba($light-slate, 0.4);
 		color: $body-text;
+		margin-bottom: 5px;
 
 		&:not(:last-of-type) {
 			margin-right: 5px;
+		}
+	}
+}
+
+.task-list {
+	li {
+
+		&:before {
+			content: '';
+			display: inline-block;
+			height: 1em;
+			width: 1em;
+			background-image: url('../assets/svgs/terminal.svg');
+			background-size: contain;
+			background-repeat: no-repeat;
+			margin-right: 0.75em;
 		}
 	}
 }
